df1685525ffb9cebaf6ee266de8a8da78f4c84c9
Tahir <tahir@sfx.co.za>
2020-02-12 08:37:49 +0200

    Updates

---
+++ precache-manifest.9ad26ee4a01768247d4ea0133c7db96f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a017c0fa6cf5dba185c806fcbc5d01",
+    "url": "/test-react-app/index.html"
+  },
+  {
+    "revision": "9b962f22bfa2ee633703",
+    "url": "/test-react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "24d370710335dae81969",
+    "url": "/test-react-app/static/js/2.09e314f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/test-react-app/static/js/2.09e314f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b962f22bfa2ee633703",
+    "url": "/test-react-app/static/js/main.ad9f30a7.chunk.js"
+  },
+  {
+    "revision": "3c0af813fac4ff6724c8",
+    "url": "/test-react-app/static/js/runtime-main.187edc21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
