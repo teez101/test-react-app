9e24d402e1cda394daaaea933963463b8f8ebcbb
Tahir <tahir@sfx.co.za>
2020-02-13 08:16:57 +0200

    Remove

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button, ButtonGroup, Card, CardBody, CardHeader} from "reactstrap";
 
 function App() {
   return (
@@ -9,23 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <Card>
-            <CardHeader>
-              <i className="fa fa-align-justify"></i><strong>Button Group</strong>
-              <div className="card-header-actions">
-                <a href="https://reactstrap.github.io/components/button-group/" rel="noreferrer noopener" target="_blank" className="card-header-action">
-                  <small className="text-muted">docs</small>
-                </a>
-              </div>
-            </CardHeader>
-            <CardBody>
-              <ButtonGroup>
-                <Button>Left</Button>
-                <Button>Middle</Button>
-                <Button>Right</Button>
-              </ButtonGroup>
-            </CardBody>
-          </Card>
+          Taahir <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
